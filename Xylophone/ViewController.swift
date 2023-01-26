7b29b27ab10a5fd65257d30f08d32e6686a911a1
Ankhe Tomer <ankhe.tomer@gmail.com>
2023-01-26 13:35:16 -0500

    firs
    first

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -1,21 +1,32 @@
-//
-//  ViewController.swift
-//  Xylophone
-//
-//  Created by Angela Yu on 28/06/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
+import AVFoundation
+import Dispatch
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func keyPressed(_ sender: UIButton) {
 
-
+        playSound(buttonNote: sender.currentTitle!)
+        print("THe note pressed is \(sender.currentTitle!)")
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            //Bring's sender's opacity back up to fully opaque.
+            sender.alpha = 1.0
+        }
+        
+    }
+   
+    func playSound(buttonNote : String)  {
+        
+        let url = Bundle.main.url(forResource: buttonNote, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
     
-
 }
-
